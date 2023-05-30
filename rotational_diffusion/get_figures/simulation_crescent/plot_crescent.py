76761892da92aa42e20bb06c680775167e75b55d
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2023-05-30 11:43:57 -0700

    Refactored crescent plotting

--- rotational_diffusion/get_figures/simulation_crescent/plot_crescent.py
+++ rotational_diffusion/get_figures/simulation_crescent/plot_crescent.py
@@ -1,59 +1,70 @@
-import datetime
 import os
+import datetime
 
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas as pd                 # for opening csv files into a nice format for plotting
+import matplotlib.pyplot as plt     # for the actual plotting
 
-top_path = os.path.join('rotational_diffusion', 'get_figures', 'simulation_crescent')
-csv_path = os.path.join(top_path, 'data', '20230508_152725_crescent_beads.csv')
-# Load the CSV data into a pandas dataframe
+## User variables:
+CSV_NAME = None                 # default None,     specify a csv file name here to use instead of the latest
+ROLLING_AVERAGE_WINDOW = 1      # default 1,        increase for smoother rolling average, 1 is just connecting points
+PLOT_DPI = 300                  # default 300,      increase for higher resolution
+SHOW_PLOT = True                # default True,     set to False if you want to save the plot without showing it
+
+## Load data:
+path_to_this_script = os.path.abspath(__file__)
+figure_dir = os.path.dirname(path_to_this_script)
+csv_dir = os.path.join(figure_dir, 'data')
+# find the latest csv file in the data directory
+csv_files = [f for f in os.listdir(csv_dir) if f.endswith('.csv')]
+csv_files.sort()
+CSV_NAME = CSV_NAME or csv_files[-1]
+csv_path = os.path.join(figure_dir, 'data', CSV_NAME)
 df = pd.read_csv(csv_path)
 
-# Group the data by "crescent_intensity" and "rotational_diffusion_time_us"
-grouped = df.groupby(["rotational_diffusion_time_ns_unpied", "crescent_intensity"])
+## Group data by rdt and crescent intensity:
+grouped = df.groupby(["sample_rdt", "crescent_intensity"])
 
-# Create a grid of subplots based on the number of "crescent_intensity" groups
-fig, axs = plt.subplots(nrows=len(df["rotational_diffusion_time_ns_unpied"].unique()), ncols=1, sharex=True, figsize=(6, 14))
-
-# Set the color cycle for different "rotational_diffusion_time_us" values
-color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
-
-# find number of unique rotational diffusion times
-num_unique_rdt = len(df["rotational_diffusion_time_ns_unpied"].unique())
+# find number of unique rotational diffusion times and crescent intensities
+num_unique_rdt = len(df["sample_rdt"].unique())
 num_unique_crescent = len(df["crescent_intensity"].unique())
 
 group_names = {7249.0: '40nm', 24465.0: '60nm', 113263.0: '100nm', 906106.0: '200nm'}
-# Plot each group on a separate subplot as a scatter plot with y error bars
+
+## Plot the data:
+# create a plot, using subplot here to keep it similar to other code
+fig, axs = plt.subplots(nrows=num_unique_rdt, ncols=1, sharex=True, figsize=(6, 14))
+
+# set the color cycle for unique sample values
+color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 for i, (name, group) in enumerate(grouped):
-    group = group[:-60]
     rotational_diffusion_time_us, crescent_intensity = name
-    ax = axs[df["rotational_diffusion_time_ns_unpied"].unique().tolist().index(rotational_diffusion_time_us)]
-    ax.errorbar(group["collection_time_point_us"], group["ratio_xy_mean"], yerr=group["ratio_xy_std"],
+    ax = axs[df["sample_rdt"].unique().tolist().index(rotational_diffusion_time_us)]
+
+    # plot each group on the same subplot as a scatter plot with y error bars
+    ax.errorbar(group["collection_time_point"], group["ratio_xy_mean"], yerr=group["ratio_xy_std"],
                 label=f"{crescent_intensity} crescent intensity", fmt='o', color=color_cycle[i % num_unique_crescent],
                 alpha=0.2)
     ax.set_ylabel(f"XY Ratio, bead size: {group_names[rotational_diffusion_time_us]}")
-    print(rotational_diffusion_time_us)
     ax.legend(loc='lower right')
 
-    # Plot a rolling average line between the data points
-    rolling_mean = group["ratio_xy_mean"].rolling(window=2, min_periods=1, center=True).mean()
-    ax.plot(group["collection_time_point_us"], rolling_mean, color=color_cycle[i % num_unique_crescent])
+    # plot a rolling average line between the data points
+    rolling_mean = group["ratio_xy_mean"].rolling(window=ROLLING_AVERAGE_WINDOW, min_periods=1, center=True).mean()
+    ax.plot(group["collection_time_point"], rolling_mean, color=color_cycle[i % num_unique_rdt])
 
-
-# Set the x-axis label
-axs[-1].set_xlabel("Collection time point (us)")
-
-# Adjust the spacing between subplots
-fig.tight_layout()
-
-# Show the plot
-plt.show()
+axs[-1].set_xlabel("Collection time point")
 
 # get datetime
 now = datetime.datetime.now()
 now = now.strftime("%Y%m%d_%H%M%S")
 
 # save the figure
-fig.savefig(os.path.join(top_path, 'plots', f'{now}_crescent.png'), dpi=300, bbox_inches='tight')
+csv_dir = os.path.join(figure_dir, 'plots')
+if not os.path.exists(csv_dir):
+    os.makedirs(csv_dir)
+fig.savefig(os.path.join(csv_dir, f'{now}-crescent.png'), dpi=PLOT_DPI, bbox_inches='tight')
 
+if SHOW_PLOT:
+    plt.show()
+else:
+    plt.close(fig)
